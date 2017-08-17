f15a5ad8c69c448d91c37441a26875e0cad09cc4
Roberto Saavedra <robertsvx@gmail.com>
2017-08-17 00:57:42 -0400

    Correccion y validacion
    Correcion de calculo de operaciones y validacion de doble punto, operadores juntos

--- js/app.js
+++ js/app.js
@@ -1,51 +1,80 @@
-var teclas = document.querySelectorAll('img');
+var keys = document.querySelectorAll('img');
 var addigit = document.getElementById('display');
 var operators = ['+', '-', '*', '/'];
+var operation = "";
+var lastoperator = "";
 //addigit.textContent = "0";
 
-for (var i = 0; i < teclas.length; i++) {
+for (var i = 0; i < keys.length; i++) {
 
-    teclas[i].addEventListener("onmousedown", reduce);
-    teclas[i].addEventListener("onmouseup", enlarge);
-    teclas[i].addEventListener("click",add);
+    keys[i].addEventListener("onmousedown", reduce);
+    keys[i].addEventListener("onmouseup", enlarge);
+    keys[i].addEventListener("click", add);
 }
 function add(e) {
     e.stopPropagation()
-    var valor = this.getAttribute('alt');
-    switch (valor) {
+    var value = this.getAttribute('alt');
+    switch (value) {
         case "on":
             addigit.textContent = "0";
+            operation = "";
+            lastoperator = "";
             break
         case "igual":
-            addigit.textContent =validate(addigit.textContent);
-            if (addigit.textContent)
-                addigit.textContent = eval(addigit.textContent);         
+            if (lastoperator != "" && !isNaN(operation)) {
+                operation = validate(operation);
+                operation += lastoperator + operation;
+                addigit.textContent = eval(operation);
+                addigit.textContent = formatNum(addigit.textContent);
+                operation = addigit.textContent;
+            } else {
+                operation += addigit.textContent;
+                operation = validate(operation);
+                addigit.textContent = eval(operation);
+                addigit.textContent = formatNum(addigit.textContent);
+                operation = addigit.textContent;
+            }
             break
         case "punto":
-            addigit.textContent += caracter(valor);
+            if (addigit.textContent.indexOf(".")==-1){
+                addigit.textContent += getCharacter(value);
+            }            
             break
         case "raiz":
-            addigit.textContent =validate(addigit.textContent);
+            addigit.textContent = validate(addigit.textContent);
             if (addigit.textContent)
-                addigit.textContent = eval(caracter(valor) + addigit.textContent + ")");
-            
+                addigit.textContent = eval(getCharacter(value) + addigit.textContent + ")");
             break
         case "signo":
-            addigit.textContent =validateSigno(addigit.textContent);                 
+            addigit.textContent = validateSigno(addigit.textContent);
             break
         default:
-            switch (addigit.textContent) {
-                case "0":
-                    addigit.textContent = caracter(valor);
-                    break
-                case "-0":
-                    addigit.textContent = "-" + caracter(valor);
-                    break
-                default:
-                    addigit.textContent += caracter(valor);
+            if (operators.indexOf(getCharacter(value)) > -1) {         
+                if (operation!=addigit.textContent){
+                    operation += addigit.textContent;
+                }                       
+                addigit.textContent = "";
+                operation += getCharacter(value);
+                lastoperator = getCharacter(value);
+            } else {
+                switch (addigit.textContent) {
+                    case "0":
+                        addigit.textContent = getCharacter(value);
+                        break
+                    case "-0":
+                        addigit.textContent = "-" + getCharacter(value);
+                        break
+                    default:
+                        if (addigit.textContent.length < 8) {
+                            addigit.textContent += getCharacter(value);
+                        }
+                        break
+                }
+
             }
+            break
     }
-    addigit.textContent=formatNum(addigit.textContent);
+    //addigit.textContent=formatNum(addigit.textContent);   
 }
 
 function reduce(e) {
@@ -60,8 +89,8 @@ function enlarge(e) {
     tecla.style = "width:" + (tecla.clientWidth + 2) + "px;height:" + (tecla.clientHeight + 2) + "px";
 }
 
-function caracter(valor) {
-    switch (valor) {
+function getCharacter(value) {
+    switch (value) {
         case "mas":
             return "+";
         case "menos":
@@ -77,33 +106,33 @@ function caracter(valor) {
         case "raiz":
             return "Math.sqrt(";
         default:
-            return valor;
+            return value;
     }
 }
 
-function formatNum(valor) {
-    if (valor.length>8){
-        valor = valor.substr(1,8);
+function formatNum(value) {
+    if (value.length > 8) {
+        value = value.substr(0, 8);
     }
-    return valor;
+    return value;
 }
 
-function validate(valor) {
-    var lastChar = valor[valor.length - 1];
-    if (operators.indexOf(lastChar) > -1 || lastChar == caracter("punto")) {
-       valor = valor.replace(/.$/, '');
+function validate(value) {
+    var lastChar = value[value.length - 1];
+    if (operators.indexOf(lastChar) > -1 || lastChar == getCharacter("punto")) {
+        value = value.replace(/.$/, '');
     }
-    return valor;
+    return value;
 }
 
-function validateSigno(valor) {
-    var firstChar = valor[0];
-    if (firstChar == caracter("signo")) {
-         valor=valor.slice(1);
-    }else{
-        valor= caracter("signo") + valor;
+function validateSigno(value) {
+    var firstChar = value[0];
+    if (firstChar == getCharacter("signo")) {
+        value = value.slice(1);
+    } else {
+        value = getCharacter("signo") + value;
     }
-    return valor;
+    return value;
 }
 
 
